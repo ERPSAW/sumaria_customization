7580f06f1dea603c561a4ba8ca0adb3dd2e6ea10
akshaykhatale <akshaykhatale78@gmail.com>
2025-04-17 18:33:30 +0530

    fix: make changes to finance and add custom fields to jv

--- sumaria_customization/overrides/payment_entry.py
+++ sumaria_customization/overrides/payment_entry.py
@@ -32,6 +32,14 @@ def create_journal_entry(doc):
         "posting_date": doc.posting_date,
         "total_debit": (amount),
         "total_credit": (amount),
+        "custom_finance_company":doc.custom_finance_company,
+        "custom_product_price":doc.custom_product_price,
+        "custom_doid":doc.custom_doid,
+        "custom_down_payment":doc.custom_down_payment,
+        "custom_dealer_interest_subsidy":doc.custom_dealer_interest_subsidy,
+        "custom_disbursement_amount":doc.custom_disbursement_amount,
+        "custom_finance_charges":doc.custom_finance_charges,
+        "custom_payment_type":"Finance"
     }
     jvdoc = frappe.get_doc(journal_entry)
     jvdoc.append(
@@ -79,6 +87,7 @@ def create_journal_entry_credit_card(doc, method=None):
         "custom_swipe_amount": doc.custom_swipe_amount,
         "custom_instant_cash_discount": doc.custom_instant_cash_discount,
         "custom_interest_subvention": doc.custom_interest_subvention,
+        "custom_payment_type": "Credit Card"
     }
     jvdoc = frappe.get_doc(journal_entry)
     terminal_provider = frappe.db.get_value(
@@ -87,19 +96,20 @@ def create_journal_entry_credit_card(doc, method=None):
     bank_account = frappe.db.get_value(
         "Terminal Master Record", doc.custom_terminal_id, "merchant_bank"
     )
-    jvdoc.append(
-        "accounts",
-        {
-            "account": frappe.db.get_value(
-                "Terminal Provider", terminal_provider, "account"
-            ),
-            "party_type": "Terminal Provider",
-            "party": terminal_provider,
-            "debit_in_account_currency": doc.custom_instant_cash_discount
-            + doc.custom_interest_subvention,
-            "branch": doc.branch,
-        },
-    )
+    if doc.custom_instant_cash_discount != 0 and doc.custom_interest_subvention != 0:
+        jvdoc.append(
+            "accounts",
+            {
+                "account": frappe.db.get_value(
+                    "Terminal Provider", terminal_provider, "account"
+                ),
+                "party_type": "Terminal Provider",
+                "party": terminal_provider,
+                "debit_in_account_currency": doc.custom_instant_cash_discount
+                + doc.custom_interest_subvention,
+                "branch": doc.branch,
+            },
+        )
     jvdoc.append(
         "accounts",
         {
